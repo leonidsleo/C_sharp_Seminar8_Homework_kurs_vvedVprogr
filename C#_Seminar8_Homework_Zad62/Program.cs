532f9ed0745b920c471c99dc703e0c880b2cb29e
leonid <leonidsleo@yandex.ru>
2023-06-05 20:17:44 +0300

    решена задача 62, дз семинар 8, курс введ в прогр

--- C#_Seminar8_Homework_Zad62/Program.cs
+++ C#_Seminar8_Homework_Zad62/Program.cs
@@ -5,3 +5,69 @@
 // 12 13 14 05
 // 11 16 15 06
 // 10 09 08 07
+
+Console.Write("Введите значение начала спирали =  ");
+int start = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[4, 4];
+int i = 0;
+int j = 0;
+while (j < array.GetLength(1))
+{
+    array[i, j] = start;
+    start = start + 1;
+    j++;
+}
+
+i++;
+j = array.GetLength(1) - 1;
+while (i < array.GetLength(0))
+{
+    array[i, j] = start;
+    start = start + 1;
+    i++;
+}
+
+i = array.GetLength(0) - 1;
+j = array.GetLength(1) - 2;
+while (j >= 0)
+{
+    array[i, j] = start;
+    start = start + 1;
+    j--;
+}
+
+i = array.GetLength(0) - 2;
+j = array.GetLength(1) - array.GetLength(1);
+while (i > 0)
+{
+    array[i, j] = start;
+    start = start + 1;
+    i--;
+}
+
+i = array.GetLength(0) - array.GetLength(0) + 1;
+j = array.GetLength(1) - array.GetLength(1) + 1;
+while (j < array.GetLength(1) - 1)
+{
+    array[i, j] = start;
+    start = start + 1;
+    j++;
+}
+
+i = array.GetLength(0) - 2;
+j = array.GetLength(1) - 2;
+while (j >= array.GetLength(1) - array.GetLength(1) + 1)
+{
+    array[i, j] = start;
+    start = start + 1;
+    j--;
+}
+
+for (i = 0; i < array.GetLength(0); i++)
+{
+    for (j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write($"{array[i, j]:D2} ");
+    }
+    Console.WriteLine();
+}
